d10d5e70f66999b768ec212936716d459513750e
Taciana Cruz <tacigomess@me.com>
2024-06-21 16:01:37 +0200

    Adding files in the new architecture

---
+++ backend/search_ideas.py
@@ -0,0 +1,8 @@
+def search_ideas_options():
+
+    frases = []
+    frases.append("What are the Species in our dataset")
+    frases.append("Pie chart of the Species")
+    frases.append("Avarage of the Value")
+
+    return frases
